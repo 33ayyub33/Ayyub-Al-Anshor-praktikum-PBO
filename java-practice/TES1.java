a13b01854370079c94b84f8e7eb85a0f4b2dbf00
ayyub <117134879+33ayyub33@users.noreply.github.com>
2024-02-21 08:12:04 +0700

    Add files via upload

---
+++ java-practice/TES1.java
@@ -0,0 +1,16 @@
+class TES1
+{
+		public static void main(String args[])
+		{
+			System.out.println("Quotes Hari Ini : Hello World!");
+
+		}
+
+}
+
+/*
+	javac TES1.java
+	(buat compile)
+	
+	java TES1
+*/
