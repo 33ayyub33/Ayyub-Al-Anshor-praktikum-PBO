7dcc5be92b45f32106bd9a8cbc414ddfa970625d
ayyub <117134879+33ayyub33@users.noreply.github.com>
2024-02-21 09:12:42 +0700

    Add files via upload

---
+++ Ayyub Al Anshor_Praktikum-PBO/Praktikum 1/MTitik.java
@@ -0,0 +1,31 @@
+class MTitik
+{
+		public static void main(String[] args)
+		{
+			Titik t1, t2, t3;
+			
+			t1 = new Titik();
+			t1.setAbsis(1);
+			t1.setOrdinat(2);
+			
+			t2 = new Titik();
+			t2.setAbsis(3);
+			t2.setOrdinat(4);
+			
+			t3 = new Titik(5,6);
+/*			System.out.println("Absis T1 = " + t1.getAbsis());
+			System.out.println("Ordinat T1 = " + t1.getOrdinat());
+
+			System.out.println("Absis T2 = " + t2.getAbsis());
+			System.out.println("Ordinat T2 = " + t2.getOrdinat());
+
+			System.out.println("Absis T3 = " + t3.getAbsis());
+			System.out.println("Ordinat T3 = " + t3.getOrdinat());
+*/
+
+			System.out.println("Jumlah Objek Titik : " + Titik.getCounter());
+			System.out.println("t1(" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
+			System.out.println("t1(" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
+			System.out.println("t1(" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
+		}
+}
