faa82084ebf52513e55acbdbc69759d68252f650
ayyub <117134879+33ayyub33@users.noreply.github.com>
2024-03-02 15:59:21 +0700

    Add files via upload

---
+++ Ayyub Al Anshor_Praktikum-PBO/Praktikum 2/Tugas Tambahan/Soal1/org/bangunruang/LimasSegiEmpat.java
@@ -0,0 +1,16 @@
+package org.bangunruang;
+import org.bangundatar.BujurSangkar;
+
+public class LimasSegiEmpat {
+    private BujurSangkar alas;
+    private double tinggi;
+
+    public LimasSegiEmpat(double sisi, double tinggi) {
+        this.alas = new BujurSangkar(sisi);
+        this.tinggi = tinggi;
+    }
+
+    public double hitungVolume() {
+        return (alas.hitungLuas() * tinggi) / 3;
+    }
+}
